804018934921c18c424b968efba002fd2294d6ea
magabriela <magalopeap@gmail.com>
2022-12-05 23:13:22 -0500

    POO y herencia ts

--- course-2/intro/ts/index.ts
+++ course-2/intro/ts/index.ts
@@ -1,1 +1,35 @@
-console.log();
+// Super set de javascript : tipos y modificadores de acceso
+
+console.log("hola desde TSc")
+class Drink {
+  private name: string
+  constructor(name : string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name
+  }
+}
+
+const drink = new Drink("Malta");
+
+console.log(drink.info());
+
+class Tea extends Drink {
+  private temp : string;
+
+  constructor (name : string, temp : string) {
+    super(name);
+
+    this.temp = temp;
+  }
+
+  info(): string {
+    return `This is a ${this.temp} ${super.info()}`; 
+  }
+}
+
+const tea = new Tea("Jink", "cold");
+
+console.log(tea.info());
